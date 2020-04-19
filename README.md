becdc3cebab722372c63c037c84ce5ea791f7304
joeygaitan <joeygaitan707@gmail.com>
2020-04-18 22:59:02 -0700

    working on finishing up homework

---
+++ README.md
@@ -0,0 +1,6 @@
+# Number base converter
+
+* convert from binary to decimal
+* convert from decimal to binary
+* convert from hexideciaml to decimal
+*convert from decima
---
+++ bases.py
@@ -0,0 +1,103 @@
+#!python
+
+import string
+# Hint: Use these string constants to encode/decode hexadecimal digits and more
+# string.digits is '0123456789'
+# string.hexdigits is '0123456789abcdefABCDEF'
+# string.ascii_lowercase is 'abcdefghijklmnopqrstuvwxyz'
+# string.ascii_uppercase is 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# string.ascii_letters is ascii_lowercase + ascii_uppercase
+# string.printable is digits + ascii_letters + punctuation + whitespace
+
+
+def decode(digits, base):
+    """Decode given digits in given base to number in base 10.
+    digits: str -- string representation of number (in given base)
+    base: int -- base of given number
+    return: int -- integer representation of number (in base 10)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    # TODO: Decode digits from binary (base 2)
+    # ...
+    # TODO: Decode digits from hexadecimal (base 16)
+    # ...
+    # TODO: Decode digits from any base (2 up to 36)
+    # ...
+    result = 0
+    #resulted
+    #reverse string input
+    reversed_string = digits[::-1]
+    for index, element in enumerate(reversed_string):
+        # imported from string
+        hexi_string = string.hexdigits
+        # hexi checker
+        hexi_checker = hexi_string.index(element.lower())
+        result += hexi_checker * (base ** index)
+    return result
+
+    
+
+
+def encode(number, base):
+    """Encode given number in base 10 to digits in given base.
+    number: int -- integer representation of number (in base 10)
+    base: int -- base to convert to
+    return: str -- string representation of number (in given base)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    # Handle unsigned numbers only for now
+    assert number >= 0, 'number is negative: {}'.format(number)
+    # TODO: Encode number in binary (base 2)
+    # ...
+    # TODO: Encode number in hexadecimal (base 16)
+    # ...
+    # TODO: Encode number in any base (2 up to 36)
+    # ...
+    encoded_result = ''
+    while number > 0:
+
+    return result
+
+
+def convert(digits, base1, base2):
+    """Convert given digits in base1 to digits in base2.
+    digits: str -- string representation of number (in base1)
+    base1: int -- base of given number
+    base2: int -- base to convert to
+    return: str -- string representation of number (in base2)"""
+    # Handle up to base 36 [0-9a-z]
+    assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
+    assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
+    # TODO: Convert digits from base 2 to base 16 (and vice versa)
+    # ...
+    # TODO: Convert digits from base 2 to base 10 (and vice versa)
+    # ...
+    # TODO: Convert digits from base 10 to base 16 (and vice versa)
+    # ...
+    # TODO: Convert digits from any base to any base (2 up to 36)
+    # ...
+    number = decode(digits, base1)
+    return encode(number, base2)
+
+def main():
+    """Read command-line arguments and convert given digits between bases."""
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 3:
+        digits = args[0]
+        base1 = int(args[1])
+        base2 = int(args[2])
+        # Convert given digits between bases
+        result = convert(digits, base1, base2)
+        print('{} in base {} is {} in base {}'.format(digits, base1, result, base2))
+    else:
+        print('Usage: {} digits base1 base2'.format(sys.argv[0]))
+        print('Converts digits from base1 to base2')
+
+
+if __name__ == '__main__':
+    # main()  
+    # print(decode())
+    # print(decode('14414', 5))
+    # print(decode('1010', 2))
+    # print(decode('B9', 16))
